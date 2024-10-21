2ec767de7e68973b5098498c0859be7f37f11b64
Martina Kurková <martinanoha@seznam.cz>
2024-10-21 18:47:05 +0200

    pauza

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-const nadpis = document.querySelector("h1");
+const nadpis = document.querySelector("h1");  //odchytávání, vždy const, nechceme měnit
+//const nadpis2 = document.querySelectorAll("h1"); teď řešit nebudeme
 
-console.log(nadpis);
+nadpis.textContent = "Toto jsem změnil pomocí JS"
+
+//změny inline stylů -  jsou přímo na html elementu
+nadpis.style.color = "yellow";
+nadpis.style.backgroundColor = "red";
+//moc se to nedělá, přebíjí styly CSS, hodí se pro testující účely, ne moc pro praxi
+
+//můžeme ovlivnit třídy na úrovni elementu
+//nekontroluje, zda je styl použit i v html a použije jej
+nadpis.classList.add("ramecek");  //přidáme třídu
+nadpis.classList.remove("ramecek");  //smažeme třídu
+
+//id
+document.querySelector("#kocka").src="https://cdn.britannica.com/34/233234-050-1649BFA9/Pug-dog.jpg?w=300";
+document.querySelector("#kocka").style.width = "200px";
+document.querySelector("#kocka").alt = "pug";
+
+document.querySelector(".produkty p");  //výběr kontextového selektoru
+
+document.getElementById //už se moc nepoužívá, query selektor je univerzálnější
+
